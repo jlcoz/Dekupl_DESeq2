7883185d62825a7d6a70c40f71b2e1cae0aa73c2
Tikal881 <lecozjeremy.etu@gmail.com>
2017-05-19 11:38:36 +0200

    Remove the case where only 1 chunk and corrected NF loading

--- DESeq2_diff_method.R
+++ DESeq2_diff_method.R
@@ -101,96 +101,21 @@ if(nb_line_last_file < (split_lines/2)){
                sep=""))
 }
 
-#LOAD THE FILENAMES OF DIFFERENT CHUNKS
+  ## LOAD THE FILENAMES OF THE DIFFERENT CHUNKS
 lst_files = system(paste("find",output_tmp_chunks,"-iname \"*_subfile.txt\" | sort -n"), intern = TRUE)
         
-sink(output_log, append=TRUE, split=TRUE)
 print(paste(Sys.time(),"Split done"))
-sink()
 
+  ## LOAD THE HEADER
 header = as.character(unlist(read.table(file = "header_no_GENCODE.txt", sep = "\t", header = FALSE)))
 
-sink(output_log, append=TRUE, split=TRUE)
-print(paste(Sys.time(),"Foreach on the",length(lst_files),"files"))
-sink()
+print(paste(Sys.time(),"Foreach on the", length(lst_files),"files"))
 
  ## LOADING PRIOR KNOWN NORMALISATION FACTORS
-size_factors = data.frame(fread(paste("cat ",normalization_factor_path," | awk '{print $1,$3}'")))
-
- ## AVOIDING ERROR WHERE THE LIST HAS ONLY ONE ELEMENT, FOREACH CRASH
-if(length(lst_files)==1){
-
-  bigTab=data.frame(fread(paste(lst_files),header=FALSE))
-  #SET TAGS AS ROWNAMES
-  rownames(bigTab)=bigTab[,1]
-  #REMOVE THE TAG AS A COLUMN
-  bigTab=bigTab[,2:ncol(bigTab)]
-  names(bigTab)=header[2:length(header)]
-
-  #FORMAT COLS DATA
-
-  colDat <- data.frame(conds=factor(c(rep(0,nb_conditionA),rep(1,nb_conditionB))))
-
-  countData = as.matrix(bigTab)
-
-  dds <- DESeqDataSetFromMatrix(countData,
-                              colData = colDat,
-                              design = ~ conds)
-
-  NormCount_names = colnames(bigTab)
-  rm(bigTab);gc()
-
-  #RUN DESEQ2 AND COLLECT DESeq2 results
-
-  #REPLACE SIZE FACTORS by SIZE FACTORS COMPUTED ON
-  #THE ALL DATASET
-
-  normFactors <- matrix(size_factors[,2],
-                        ncol=ncol(dds),nrow=nrow(dds),
-                        dimnames=list(1:nrow(dds),1:ncol(dds)),
-                        byrow = TRUE)
-                        
-  normalizationFactors(dds) <- normFactors
-
-  #RUN DESeq2
-  dds <- estimateDispersionsGeneEst(dds)
-  dds <- estimateDispersionsFit(dds)
-  dds <- estimateDispersionsMAP(dds)
-  dds <- nbinomWaldTest(dds)
-  resDESeq2 <- results(dds, pAdjustMethod = "none")
-
-  #COLLECT COUNTS
-  NormCount<- as.data.frame(counts(dds, normalized=TRUE))
-  names(NormCount) <- NormCount_names
-
-  # WRITE A TSV WITH THIS FORMAT FOR THE CURRENT CHUNK
-  # Kmer_ID
-  # meanA
-  # meanB
-  # log2FC
-  # NormCount
-
-  write.table(data.frame(ID=rownames(resDESeq2),
-                         meanA=rowMeans(NormCount[,1:nb_conditionA]),
-                         meanB=rowMeans(NormCount[,(as.numeric(nb_conditionA)+1):(as.numeric(nb_conditionA)+as.numeric(nb_conditionB))]),
-                         log2FC=resDESeq2$log2FoldChange,
-                         NormCount),
-              file=paste(output_tmp_DESeq2,"1_dataDESeq2_part_tmp", sep=""),
-              sep="\t",quote=FALSE,
-              row.names = FALSE,
-              col.names = TRUE)
-
-  # WRITE PVALUES FOR THE CURRENT CHUNK
-  write.table(data.frame(ID=rownames(resDESeq2),pvalue=resDESeq2$pvalue),
-                file=paste(output_tmp_DESeq2,"1_pvalue_part_tmp",sep=""),
-                sep="\t",quote=FALSE,
-                row.names = FALSE,
-                col.names = FALSE)
-
-}else{
+size_factors = data.frame(fread(paste("cat ",normalization_factor_path," | awk '{print $1,$3}'",sep="")))
 #DESeq2 ANALYSIS ON EACH CHUNKS
-  invisible(foreach(i=1:length(lst_files)) %dopar%{
 
+invisible(foreach(i=1:length(lst_files)) %dopar%{
     bigTab=data.frame(fread(paste(lst_files[i]),header=FALSE))
     #SET TAGS AS ROWNAMES
     rownames(bigTab)=bigTab[,1]
@@ -214,7 +139,6 @@ if(length(lst_files)==1){
 
     #REPLACE SIZE FACTORS by SIZE FACTORS COMPUTED ON
     #THE ALL DATASET
-
     normFactors <- matrix(size_factors[,2],
                         ncol=ncol(dds),nrow=nrow(dds),
                         dimnames=list(1:nrow(dds),1:ncol(dds)),
@@ -256,8 +180,8 @@ if(length(lst_files)==1){
                 sep="\t",quote=FALSE,
                 row.names = FALSE,
                 col.names = FALSE)
-  }) #END FOREACH
-} #END ELSE
+
+}) #END FOREACH
 
 sink(output_log, append=TRUE, split=TRUE)
 print(paste(Sys.time(),"Foreach done"))
