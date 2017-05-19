55522a5a0c5fab5aed613f4dab2fde0b47ce18c1
Tikal881 <lecozjeremy.etu@gmail.com>
2017-05-19 21:19:17 +0200

    Sink corrected

--- Ttest_diff_method.R
+++ Ttest_diff_method.R
@@ -1,21 +1,23 @@
 Ttest=snakemake@input$Ttest_filter
 no_GENCODE=snakemake@input$counts
+working_dir = snakemake@input$kmer_DE_dir 
 normalization_factor_path = snakemake@input$sample_conditions
 pvalue_threshold=snakemake@config$Ttest$pvalue_threshold
 log2fc_threshold=snakemake@config$Ttest$log2fc_threshold
 
 output_diff_counts=snakemake@output$diff_counts
-output_pvalue_all=snakemake@output$pvalue_all
+#output_pvalue_all=snakemake@output$pvalue_all
 
 output_log=snakemake@log[[1]]
 
-sink(output_log, append=TRUE, split=TRUE)
+setwd(working_dir)
+
+sink(file=paste(output_log), append=TRUE, split=TRUE)
 print(paste(Sys.time(),"Start Ttest_diff_methods"))
 sink()
 
 system(paste(Ttest," -p ",pvalue_threshold, " -f ",log2fc_threshold," ",no_GENCODE," ",normalization_factor_path," A B | gzip -c > ",output_diff_counts,sep=""))
-system(paste("mv raw_pvals.txt ",output_pvalue_all,sep=""))
 
-sink(output_log, append=TRUE, split=TRUE)
+sink(file=paste(output_log), append=TRUE, split=TRUE)
 print(paste(Sys.time(),"End Ttest_diff_methods"))
 sink()
